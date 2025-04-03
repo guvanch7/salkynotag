874e4b9e60c5e72a1e0f207fff82cdc39a0592f1
guvanch7 <k.guvanch7@gmail.com>
2025-04-03 11:19:50 +0500

    Исправил API URL для продакшена

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -16,7 +16,8 @@ import hammer from '../assets/icon/hammer.png'
 import tick from '../assets/icon/tick.png'
 import shield from '../assets/icon/shield.png'
 
-import bg from '../assets/pages/bgTemplate.png'
+import bg1 from '../assets/pages/bgTemplate1.png'
+import bg2 from '../assets/pages/bgTemplate2.png'
 
 
 
@@ -42,40 +43,43 @@ const About = () => {
 
             <MDBContainer fluid style={{
 
-                backgroundImage: `url(${bg})`,
-                backgroundSize: 'cover',  // Изображение будет растягиваться на весь блок
+                backgroundImage: `url(${bg1})`,
+                height: '1050px ',
+
+                backgroundSize: 'cover',
+                // Изображение будет растягиваться на весь блок
                 backgroundPosition: 'top center',  // Центрирует изображение
                 backgroundRepeat: 'no-repeat',  // Отключает повторение изображения
             }} className=' bg-image '>
-                <MDBRow className='align-items-stretch justify-content-between'>
+                <MDBRow className='align-items-stretch mt-7 justify-content-between'>
                     <MDBCol className='px-4 mt-7' lg={7}>
-                        <h1 className='mt-7 ps-3 display-3 fw-bold text-white'>Наши услуги</h1>
-                        <MDBRow className='mt-5 mt-7 ps-3 text-white'>
+                        <h1 className='mt-7 ps-3  fw-bold text-white'>Наши услуги</h1>
+                        <MDBRow className='mt-5 mt-7 ps-3 text-white align-items-end'>
                             <MDBCol className='p-3' data-aos="fade-up" data-aos-duration="500" lg={6}>
                                 <div className="d-flex align-items-start ">
-                                    <img src={cart} className='img-fluid pe-2 pt-1' style={{ width: "4rem" }} />
+                                    <img src={cart} className='img-fluid pe-2 pt-1' style={{ width: "3.5rem" }} />
                                     <div>
-                                        <h1>Продажа кондиционеров</h1>
-                                        <p className='fw-light fs-3'>Бытовые и промышленные системы кондиционирования различной мощности.</p>
+                                        <h2>Продажа кондиционеров</h2>
+                                        <p className='fw-light  fs-5'>Бытовые и промышленные системы кондиционирования различной мощности.</p>
                                     </div>
                                 </div>
                             </MDBCol>
                             <MDBCol className='p-3' data-aos="fade-up" data-aos-duration="1000" lg={6}>
                                 <div className="d-flex align-items-start ">
-                                    <img src={tool} className='img-fluid pe-2 pt-1' style={{ width: "4rem" }} />
+                                    <img src={tool} className='img-fluid pe-2 pt-1' style={{ width: "3.5rem" }} />
                                     <div>
-                                        <h1>Монтаж и установка</h1>
-                                        <p className='fw-light fs-3'>Профессиональный монтаж с соблюдением всех технических норм.</p>
+                                        <h2>Монтаж и установка</h2>
+                                        <p className='fw-light fs-5'>Профессиональный монтаж с соблюдением всех технических норм.</p>
                                     </div>
                                 </div>
 
                             </MDBCol>
                             <MDBCol className='p-3' data-aos="fade-up" data-aos-duration="1500" lg={6}>
                                 <div className="d-flex align-items-start ">
-                                    <img src={setting} className='img-fluid pe-2 pt-1' style={{ width: "4rem" }} />
+                                    <img src={setting} className='img-fluid pe-2 pt-1' style={{ width: "3.5rem" }} />
                                     <div>
-                                        <h1>Сервис и ремонт</h1>
-                                        <p className='fw-light fs-3'>Диагностика, профилактика заправка и полное восстановление систем.</p>
+                                        <h2>Сервис и ремонт</h2>
+                                        <p className='fw-light fs-5'>Диагностика, профилактика заправка и полное восстановление систем.</p>
                                     </div>
                                 </div>
 
@@ -84,75 +88,88 @@ const About = () => {
 
                             <MDBCol className='p-3' data-aos="fade-up" data-aos-duration="2000" lg={6}>
                                 <div className="d-flex align-items-start ">
-                                    <img src={speech} className='img-fluid pe-2 pt-1' style={{ width: "4rem" }} />
+                                    <img src={speech} className='img-fluid pe-2 pt-1' style={{ width: "3.5rem" }} />
                                     <div>
-                                        <h1>Консультации</h1>
-                                        <p className='fw-light fs-3'>Помощь в выборе оптимального оборудования для ваших потребностей.</p>
+                                        <h2>Консультации</h2>
+                                        <p className='fw-light fs-5'>Помощь в выборе оптимального оборудования для ваших потребностей.</p>
                                     </div>
                                 </div>
                             </MDBCol>
                         </MDBRow>
                     </MDBCol>
+                </MDBRow>
+            </MDBContainer>
 
-                    <MDBCol className='ps-5' lg={7} style={{ paddingTop: '1350px' }}>
-                        <h1 className='text-white ps-5 py-4'>Процесс работы</h1>
 
+            <MDBContainer fluid style={{
+                backgroundImage: `url(${bg2})`,
+                height: '1600px',
+                backgroundSize: 'cover',
+                // Изображение будет растягиваться на весь блок
+                backgroundPosition: 'top center',  // Центрирует изображение
+                backgroundRepeat: 'no-repeat',  // Отключает повторение изображения
+            }} className='bg-image'>
+                <MDBRow className='justify-content-end mt-7'>
+                    <MDBCol className="pe-4 text-end" lg={4}>
+                        <h1 className="text-white  py-4">Процесс работы</h1>
 
-                        <div data-aos="fade-right" data-aos-duration="500" className='d-flex align-items-center'>
-                            <div class="icon-container me-3">
-                                <img src={phone} style={{ width: '2.5rem' }} className='img-fluid' alt="Phone Icon" />
-                            </div>
-                            <div className='text-white'>
+                        <div data-aos="fade-left" data-aos-duration="500" className="d-flex align-items-center justify-content-end">
+                            <div className="text-white">
                                 <h4>Консультация</h4>
                                 <p>Бесплатный выезд и оценка объекта.</p>
                             </div>
+                            <div className="icon-container ms-3">
+                                <img src={phone} style={{ width: "2.5rem" }} className="img-fluid" alt="Phone Icon" />
+                            </div>
                         </div>
 
-                        <div data-aos="fade-right" data-aos-duration="1000" className='d-flex align-items-center'>
-                            <div class="icon-container me-3">
-                                <img src={glass} style={{ width: '2.5rem' }} className='img-fluid' alt="Phone Icon" />
-                            </div>
-                            <div className='text-white'>
+                        <div data-aos="fade-left" data-aos-duration="1000" className="d-flex align-items-center justify-content-end">
+                            <div className="text-white">
                                 <h4>Подбор</h4>
                                 <p>Оптимальное оборудование для ваших задач.</p>
                             </div>
+                            <div className="icon-container ms-3">
+                                <img src={glass} style={{ width: "2.5rem" }} className="img-fluid" alt="Glass Icon" />
+                            </div>
                         </div>
 
-                        <div data-aos="fade-right" data-aos-duration="1500" className='d-flex align-items-center'>
-                            <div class="icon-container me-3">
-                                <img src={hammer} style={{ width: '2.5rem' }} className='img-fluid' alt="Phone Icon" />
-                            </div>
-                            <div className='text-white'>
+                        <div data-aos="fade-left" data-aos-duration="1500" className="d-flex align-items-center justify-content-end">
+                            <div className="text-white">
                                 <h4>Монтаж</h4>
                                 <p>Профессиональная установка системы.</p>
                             </div>
+                            <div className="icon-container ms-3">
+                                <img src={hammer} style={{ width: "2.5rem" }} className="img-fluid" alt="Hammer Icon" />
+                            </div>
                         </div>
 
-                        <div data-aos="fade-right" data-aos-duration="2000" className='d-flex align-items-center'>
-                            <div class="icon-container me-3">
-                                <img src={tick} style={{ width: '2.5rem' }} className='img-fluid' alt="Phone Icon" />
-                            </div>
-                            <div className='text-white'>
+                        <div data-aos="fade-left" data-aos-duration="2000" className="d-flex align-items-center justify-content-end">
+                            <div className="text-white">
                                 <h4>Пусконаладка</h4>
                                 <p>Тестирование и ввод в эксплуатацию.</p>
                             </div>
+                            <div className="icon-container ms-3">
+                                <img src={tick} style={{ width: "2.5rem" }} className="img-fluid" alt="Tick Icon" />
+                            </div>
                         </div>
 
-                        <div data-aos="fade-right" data-aos-duration="2500" className='d-flex align-items-center'>
-                            <div class="icon-container me-3">
-                                <img src={shield} style={{ width: '2.5rem' }} className='img-fluid' alt="Phone Icon" />
-                            </div>
-                            <div className='text-white'>
+                        <div data-aos="fade-left" data-aos-duration="2500" className="d-flex align-items-center justify-content-end">
+                            <div className="text-white">
                                 <h4>Обслуживание</h4>
                                 <p>Гарантийный и постгарантийный сервис.</p>
                             </div>
+                            <div className="icon-container ms-3">
+                                <img src={shield} style={{ width: "2.5rem" }} className="img-fluid" alt="Shield Icon" />
+                            </div>
                         </div>
-
                     </MDBCol>
 
 
-                    <MDBCol lg={12} className='text-white'>
-                        <h1 className='mt-7'>Свяжитесь с нами</h1>
+
+
+                    <MDBCol lg={12} className='text-white mt-7'>
+
+                        <h1 className='mt-7 text-center'>Свяжитесь с нами</h1>
                         <MDBRow className='justify-content-center mt-5 text-center'>
 
                             <MDBCol data-aos="fade-up" data-aos-duration="500" lg={6}>
